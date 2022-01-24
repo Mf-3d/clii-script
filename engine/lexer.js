008bf4c9683f58789bef069d63988661e2edcc6e
mf-3d <84224913+Mf-3d@users.noreply.github.com>
2022-01-24 22:05:05 +0900

    Delete engine directory

--- engine/lexer.js
+++
@@ -1,5 +0,0 @@
-exports.lexer = function (filename) {
-  var source = filename;
-  source = source.replace(/^#!.*?\n/,"");
-  return source.split(/\/\/.*$|(".*?"|\d+(?:\.\d+){0,1}|\w+)|\s|(.)/m).filter(a=>a);
-}
